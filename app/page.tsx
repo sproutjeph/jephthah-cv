82bb7077c664a927ff01f3dee63655dcd55a952e
jephthah <jephthah.mbah@outlook.com>
2024-01-29 09:47:52 +0100

    print fornce new page

--- app/page.tsx
+++ app/page.tsx
@@ -157,14 +157,14 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
+          <h2 className="text-xl font-bold print-force-new-page">Skills</h2>
           <div className="flex flex-wrap gap-1">
             {RESUME_DATA.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
             })}
           </div>
         </Section>
-        <Section className="print-force-new-page scroll-mb-16">
+        <Section className="scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="grid grid-cols-1 gap-3 -mx-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
