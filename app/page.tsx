05d9182f08a239d378eab84836778715899cb78c
jephthah <jephthah.mbah@outlook.com>
2024-01-29 09:40:38 +0100

    fixed page output

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,6 @@ export default function Page() {
             })}
           </div>
         </Section>
-
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="grid grid-cols-1 gap-3 -mx-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
